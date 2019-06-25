d1405762274cf3f094165b38aa07d1624cd37e5f
Yilin-Y <52220650+Yilin-Y@users.noreply.github.com>
2019-06-25 18:51:38 -0400

    Add files via upload

---
+++ Module01/hello_world.py
@@ -0,0 +1,10 @@
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(' Hello world  ')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print()
+
+user_input = input('What is your name?')
+reply = 'Nice to meet you ' + user_input
+
+print(reply)
+
