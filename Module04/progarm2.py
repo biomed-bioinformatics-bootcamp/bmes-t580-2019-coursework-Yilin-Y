30633628a68657a6769e1ac3cdf996c4b1b297f7
WangYilin <Genie@WangYilindeMacBook-Pro.local>
2019-07-11 14:38:23 -0400

    adding the program two

---
+++ Module04/progarm2.py
@@ -0,0 +1,11 @@
+print("1. About to import program")
+
+import progarm
+# import the progarm (the error typo for the coding but it is the name of the code that I made before)
+
+print("2. Program imported")
+
+print("3. Printing header!")
+progarm.print_header() # printed the header
+print("4. Done with program.")
+
